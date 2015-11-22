88f22d9092e6be1bc96b2ccc7f0a2c654678666b
GustavoAC <guga.a.carvalho@gmail.com>
2015-11-22 19:04:56 -0300

    Update functions.h

--- ptp-work/functions.h
+++ ptp-work/functions.h
@@ -1,4 +1,4 @@
-void header(){
+void intro(){
 	system("clear");
 	printf("Bem vindo ao programa!\n");
 	printf("Dígite o nome do seu arquivo (sem extensão)");
@@ -26,14 +26,14 @@ void read_image(Pixel image[lat][lat], FILE *imagepath){
 void create_new_file(Pixel image[lat][lat]){
 	int i, j;
 	FILE *newfile;
-	newfile = fopen(nome, "w");
+	newfile = fopen(name, "w");
 	fprintf(newfile, "%s\n%i %i\n%i\n", filetype, width, height, clrRange);
 	for(i = 0; i < height; i++)
 		for(j = 0; j < width; j++)
         		fprintf(newfile, "%i %i %i ", image[i][j].red, image[i][j].green, image[i][j].blue);
 
     system("clear");
-    printf("O arquivo %s foi criado com sucesso na pasta raíz do programa.\n", nome);
+    printf("O arquivo %s foi criado com sucesso na pasta raíz do programa.\n", name);
 }
 
 void null_matrix(Pixel image[lat][lat]){
@@ -46,40 +46,40 @@ void null_matrix(Pixel image[lat][lat]){
 		}
 }
 
-void controle_rotacao(Pixel image[lat][lat]){
+void rotation_controller(Pixel image[lat][lat]){
 	int ang;
 	printf("Digite o angulo de rotação (em sentido horario):\n");
 	scanf("%i", &ang);
 	switch (ang){
 		case 90:
-			strcat(nome, "_rot_90.ppm");
+			strcat(name, "_rot_90.ppm");
 			ef_rotate_left(image);
 			ef_rotate_left(image);
 			ef_rotate_left(image);
 			break;
 		case 180:
-			strcat(nome, "_rot_180.ppm");
+			strcat(name, "_rot_180.ppm");
 			ef_rotate_left(image);
 			ef_rotate_left(image);
 			break;
 		case 270:
-			strcat(nome, "_rot_270.ppm");
+			strcat(name, "_rot_270.ppm");
 			ef_rotate_left(image);
 			break;
 		default:
 			system("clear");
 			printf("Escolha um angulo reto: 90 - 180 - 270 \n");
-			controle_rotacao(image);			
+			rotation_controller(image);			
 	}
 }
 
 void choose_options(Pixel image[lat][lat]){
-	int escolha;
+	int choice;
 	
 	printf("\n======== Escolha uma opção =======\n");
 	printf("1 - thr     //    Thresholding   *\n");
 	printf("2 - blur    //    Blurring       *\n");
-	printf("3 - sha     //    Sharpennig     *\n");
+	printf("3 - sha     //    Sharpening     *\n");
 	printf("4 - rot     //    Rotacionar     *\n");
 	printf("5 - amp     //    Ampliar        *\n");
 	printf("6 - red     //    Reduzir        *\n");
@@ -93,61 +93,83 @@ void choose_options(Pixel image[lat][lat]){
 	printf("14 - sep    //    Tom de sepia   *\n");
 	printf("==================================\n");
 
-	scanf("%i", &escolha);
-	controller(escolha, image);
+	scanf("%i", &choice);
+	controller(choice, image);
 }
 
-void controller(int opcao, Pixel image[lat][lat]){
-	if(opcao == THR){
-		int grau;
-		printf("Insira o padrão de binarização entre 0 e 255 (Padrão = 127)\n");
-		scanf("%i", &grau);
-		sprintf(nomeArq, "_thr_%i.ppm", grau);
-		strcat(nome, nomeArq);
-		ef_thresholding(image, grau);
-	}else if(opcao == BW){
-		strcat(nome, "_bw.ppm");
-		ef_black_white(image);
-	}else if(opcao == RED){
-		int grau;
-		printf("Quantas vezes deseja reduzir a imagem?\n");
-		scanf("%i", &grau);
-		sprintf(nomeArq, "_red_%i.ppm", grau);
-		strcat(nome, nomeArq);
-		ef_reduzir(image, grau);
-	}else if(opcao == BLU){
-		strcat(nome, "_blur.ppm");
-		ef_blur(image);
-	}else if(opcao == ROTL){
-		system("clear");
-		controle_rotacao(image);
-	}else if(opcao == MIR){
-		strcat(nome, "_mir.ppm");
-		ef_mirror_horizontal(image);
-	}else if(opcao == SHAR){
-		strcat(nome, "_shar.ppm");
-		ef_sharpening(image);
-	}else if(opcao == AMP){
-		int grau;
-		printf("Quantas vezes deseja ampliar a imagem?\n");
-		scanf("%i", &grau);
-		sprintf(nomeArq, "_amp_%i.ppm", grau);
-		strcat(nome, nomeArq);
-		ef_ampliar(image, grau);
-	}else if(opcao == BORDA){
-		strcat(nome, "_bordas.ppm");
-		ef_detecta_borda(image);
-	}else if(opcao == INV){
-		strcat(nome, "_neg.ppm");
-		ef_inverte_cores(image, clrRange);
-	}else if(opcao == SEP){
-		strcat(nome, "_sepia.ppm");
-		ef_sepia(image);
-	}else{
-		system("clear");
-		printf("Essa opcao nao existe, tente novamente.\n\n");
+void controller(int choice, Pixel image[lat][lat]){
+	switch(choice){
+		case THR:
+			int grau;
+			printf("Insira o padrão de binarização entre 0 e 255 (Padrão = 127)\n");
+			scanf("%i", &grau);
+			sprintf(nameFile, "_thr_%i.ppm", grau);
+			strcat(name, nameFile);
+			ef_thresholding(image, grau);
+			break;
 
-		choose_options(image);
+		case BLU:
+			strcat(name, "_blur.ppm");
+			ef_blur(image);
+			break;
 
+		case SHAR:
+			strcat(name, "_shar.ppm");
+			ef_sharpening(image);
+			break;
+
+		case ROT:
+			system("clear");
+			rotation_controller(image);
+			break;
+
+		case AMP:
+			int grau;
+			printf("Quantas vezes deseja ampliar a imagem?\n");
+			scanf("%i", &grau);
+			sprintf(nameFile, "_amp_%i.ppm", grau);
+			strcat(name, nameFile);
+			ef_enlarge(image, grau);
+			break;
+
+		case RED:
+			int grau;
+			printf("Quantas vezes deseja reduzir a imagem?\n");
+			scanf("%i", &grau);
+			sprintf(nameFile, "_red_%i.ppm", grau);
+			strcat(name, nameFile);
+			ef_reduce(image, grau);
+			break;
+
+		case BW:
+			strcat(name, "_bw.ppm");
+			ef_black_white(image);
+			break;
+
+		case MIR:
+			strcat(name, "_mir.ppm");
+			ef_horizontal_mirror(image);
+			break;
+
+		case BORD:
+			strcat(name, "_bordas.ppm");
+			ef_border_detection(image);
+			break;
+
+		case INV:
+			strcat(name, "_neg.ppm");
+			ef_negative(image, clrRange);
+			break;
+
+		case SEP:
+			strcat(name, "_sepia.ppm");
+			ef_sepia(image);
+			break;
+
+		default:
+			system("clear");
+			printf("Essa opcao nao existe, tente novamente.\n\n");
+			choose_options(image);
+			break;
 	}
 }
