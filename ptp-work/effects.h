9131cad3b6bebb1ee5100d026b125febeed5e78b
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2015-11-23 15:56:17 -0300

    Adicionado comentários

--- ptp-work/effects.h
+++ ptp-work/effects.h
@@ -1,3 +1,10 @@
+/**
+ * @brief effects.h possui todos os filtros que processam a imagem.
+ */
+
+/**
+ * A função "ef_thresholding"
+ * A imagem é binarizada, ficando apenas com duas cores: o branco e o preto*/
 void ef_thresholding(Pixel image[lat][lat], int value){
 	int i, j, gray;
 	for(i = 0; i < height; i++)
@@ -18,11 +25,18 @@ void ef_thresholding(Pixel image[lat][lat], int value){
     create_new_file(image);
 }
 
+/**
+ * A função "ef_blur"
+ * Borra a imagem e diminui a percepção dos detalhes */
 void ef_blur(Pixel image[lat][lat]){
 	int i, j;
-	Pixel temp[lat][lat];
+	
+	Pixel temp[lat][lat]; /* < "Pixel temp" é uma matriz temporária que irão receber a nova imagem, borrada */
+	
+	/* O laço passeia pela matriz de pixels*/
 	for(i = 1; i < lat-1; i++)
 		for(j = 1; j < lat-1; j++){
+				/* A matriz temporária soma os pixel ao imediatamente redor do atual pixel do laço e tira a média deles.*/
 				temp[i][j].red = (
 					image[i-1][j-1].red + image[i-1][j].red + image[i-1][j+1].red +
 					image[i][j-1].red + image[i][j].red + image[i][j+1].red +
@@ -39,6 +53,7 @@ void ef_blur(Pixel image[lat][lat]){
 					image[i+1][j-1].blue + image[i+1][j].blue + image[i+1][j+1].blue)/9;
 
 		}
+	/* O segundo laço transporta todos os novos valores da matriz temporária novamente para a matriz "image".*/
 	for(i = 1; i < lat-1; i++)
 		for(j = 1; j < lat-1; j++)
 			image[i][j] = temp[i][j];
@@ -46,11 +61,19 @@ void ef_blur(Pixel image[lat][lat]){
 	create_new_file(image);
 }
 
+/**
+ * A função "ef_sharpening"
+ * Aumenta a clareza de detalhes e das bordas*/
 void ef_sharpening(Pixel image[lat][lat]){
 	int i, j;
-	Pixel temp[lat][lat];
+	
+	Pixel temp[lat][lat];/* < Pixel temp é uma matriz temporária, que será usada para receber a imagem com as alterações*/
+	
+	/* O laço passei pela matriz de pixels.*/
 	for(i = 1; i < lat-1; i++)
 		for(j = 1; j < lat-1; j++){
+				/* O pixel "central" é multiplicado por 5, enquanto os pixel imediatamente ao lado e em cima são
+				 * multiplicados por -1 e somado ao pixel central.*/
 				temp[i][j].red = (image[i][j].red*5 - image[i-1][j].red - image[i][j-1].red - image[i][j+1].red - 
 					image[i+1][j].red);
 				temp[i][j].red = (temp[i][j].red < 255) ? temp[i][j].red : 255; 
@@ -66,7 +89,12 @@ void ef_sharpening(Pixel image[lat][lat]){
 				temp[i][j].blue = (temp[i][j].blue < 255) ? temp[i][j].blue : 255; 
 				temp[i][j].blue = (temp[i][j].blue > 0) ? temp[i][j].blue : 0;
 				
+				/* Os operadores ternários verificam se os novos valores são maiores que 255 ou menores que 0.
+				 * Caso seja maior que 255, o valor novo será o próprio 255.
+				 * Caso seja menor que 0, o valor será o pŕoprio 0.
+				 * Isso evita que os valores dos pixels saiam do color ranger e quebrem a imagem */
 	}
+	/* O segundo laço transporta todos os novos valores da matriz temporária novamente para a matriz "image".*/
 	for(i = 1; i < lat-1; i++)
 		for(j = 1; j < lat-1; j++)
 			image[i][j] = temp[i][j];
@@ -74,6 +102,9 @@ void ef_sharpening(Pixel image[lat][lat]){
 
 }
 
+/**
+ * A função "ef_rotate_left"
+ * A função rotaciona a imagem para a esquerda em 90 graus*/
 void ef_rotate_left(Pixel image[lat][lat]){
 	int i, j;
 	Pixel temp[lat][lat];
@@ -94,6 +125,9 @@ void ef_rotate_left(Pixel image[lat][lat]){
 	create_new_file(image);
 }
 
+/**
+ * A função "ef_enlarge"
+ * Dado um coeficiente de ampliação, a função amplia a imagem*/
 void ef_enlarge(Pixel image[lat][lat], int value){
 	int i, j, k, l;
 	
@@ -111,6 +145,9 @@ void ef_enlarge(Pixel image[lat][lat], int value){
     printf("O arquivo %s foi criado com sucesso na pasta raíz do programa.\n", name);
 }
 
+/**
+ * A função "ef_reduce"
+ * Dado um coeficiente de redução, a função diminui a imagem*/
 void ef_reduce(Pixel image[lat][lat], int value){
 	int i, j, k, l, lat2 = lat/value;
 	Pixel temp[lat2][lat2];
@@ -143,12 +180,18 @@ void ef_reduce(Pixel image[lat][lat], int value){
 	create_new_file(temp);
 }
 
+/**
+ * A função "ef_black_white"
+ * Deixa a imagem em escala de cinza (preto e branco).*/
+
 void ef_black_white(Pixel image[lat][lat]){
 	int i, j, gray;
-
+	
+	/* O laço passeia pela matriz de pixels*/
 	for(i = 0; i < height; i++)
 		for(j = 0; j < width; j++){
-			gray = (image[i][j].red + image[i][j].green + image[i][j].blue)/3;
+			gray = (image[i][j].red + image[i][j].green + image[i][j].blue)/3;	/* < "gray" soma os valores RGB e tira a média deles*/
+			/* Os três valores RGB recebem "gray", tornando a imagem cinza*/
 			image[i][j].red = gray;
 			image[i][j].green = gray;
 			image[i][j].blue = gray;
@@ -156,12 +199,17 @@ void ef_black_white(Pixel image[lat][lat]){
 	create_new_file(image);
 }
 
+/**
+ * A função "ef_horizontal_mirror"
+ * Espelha a imagem. */
 void ef_horizontal_mirror(Pixel image[lat][lat]){
 	int i, j;	
-	Pixel temp[lat][lat];
-	
+	Pixel temp[lat][lat]; /* < Pixel temp é uma matriz temporária, que será usada para receber a imagem espelhada*/
+	/* O laço percorre a matriz de pixels*/
 	for(i = 0; i < height; i++){
 		for(j = 0; j < width; j++){
+			/* A matriz "temp" recebe a imagem do atual [i][j] no seu oposto a matriz "image", ou seja, [tamanho - i] e
+			 * [tamanho - j]. Como a matriz começa em [0][0], o final da matriz temporária é o [tamanho - 1].*/
 			temp[i][width-j-1].red = image[i][j].red;
 			temp[i][width-j-1].green = image[i][j].green;
 			temp[i][width-j-1].blue = image[i][j].blue;
@@ -171,14 +219,21 @@ void ef_horizontal_mirror(Pixel image[lat][lat]){
 	create_new_file(temp);
 }
 
+/**
+ * A função "ef_border_detection"
+ * Essa função analiza as trocas de luminosidade da imagem e detecta as bordas*/
 void ef_border_detection(Pixel image[lat][lat]){
 	int i, j;
-	Pixel temp[lat][lat];
+	Pixel temp[lat][lat]; /* < Pixel temp é uma matriz temporária*/
 	
-	null_matrix(temp);
-
+	null_matrix(temp); /* Chama a matriz que zera todos os valores da matriz temporária*/
+	
+	/* O laço percorre a matriz de pixels*/
 	for(i = 1; i < lat-1; i++)
 		for(j = 1; j < lat-1; j++){
+				/* O pixel central é multiplicado por 8 e os imediatamente ao seu lado por -1. 
+				 * Ao final, o atual pixel da matriz temporária será a soma do pixel central e 
+				 * dos imadiatamente ao seu lado (incluindo diagonais)*/
 				temp[i][j].red =
 					(image[i][j].red*8) - image[i-1][j-1].red - image[i-1][j].red -
 					image[i-1][j+1].red - image[i][j-1].red - image[i][j+1].red -
@@ -199,17 +254,26 @@ void ef_border_detection(Pixel image[lat][lat]){
 					image[i+1][j-1].blue - image[i+1][j].blue - image[i+1][j+1].blue;
 				temp[i][j].blue = (temp[i][j].blue < 0) ? 0 : temp[i][j].blue;
 				temp[i][j].blue = (temp[i][j].blue > 255) ? 255 : temp[i][j].blue;
-
+				
+				/* Os operadores ternários verificam se os novos valores são maiores que 255 ou menores que 0.
+				 * Caso seja maior que 255, o valor novo será o próprio 255.
+				 * Caso seja menor que 0, o valor será o pŕoprio 0.
+				 * Isso evita que os valores dos pixels saiam do color ranger e quebrem a imagem */
 		}
 	
 	ef_thresholding(temp, 127);
 }
 
+/**
+ * A função "ef_negative"
+ * Negativa a imagem, recebendo o color range e subtraindo-o do atual valor do pixel*/
 void ef_negative(Pixel image[lat][lat], int clrRange){
 	int i, j;
 	
+	/* O laço percorre a matriz de pixels*/
 	for(i = 0; i < height; i++){
 		for(j = 0; j < width; j++){
+			/* Retira o atual valor do pixel do color range*/
 			image[i][j].red = clrRange - image[i][j].red;
 			image[i][j].green = clrRange - image[i][j].green;
 			image[i][j].blue = clrRange - image[i][j].blue; 
@@ -218,9 +282,13 @@ void ef_negative(Pixel image[lat][lat], int clrRange){
 	create_new_file(image);
 }
 
+/**
+ * A função "ef_sepia"
+ * Multiplica a matriz de pixel por valores pŕe-determinados para dar um tom marrom, envelhencendo a imagem*/
 void ef_sepia(Pixel image[lat][lat]){
 	int i, j;
 	
+	/* O laço passeia pela matriz de pixels*/
 	for(i = 0; i < height; i++){
 		for(j = 0; j < width; j++){
 			image[i][j].red = (image[i][j].red * 0.493) + (image[i][j].green * 0.469) + (image[i][j].blue * 0.089);
@@ -231,7 +299,7 @@ void ef_sepia(Pixel image[lat][lat]){
 
 			image[i][j].blue = (image[i][j].red * 0.272) + (image[i][j].green * 0.334) + (image[i][j].blue * 0.131);
 			
-
+			/** Os valores são pré determinados, por meio de testes, para deixar a imagem com tons de marrom/amarelo*/
 
 		}
 	}
